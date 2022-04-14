4107bc23c3209e40b15258c3c48b0cebef2d7ae7
szandany <szandany@datical.com>
2022-04-14 11:35:33 -0500

    added new project files

---
+++ changelog.sql
@@ -0,0 +1,62 @@
+-- liquibase formatted sql
+
+-- changeset Tsvi:1
+CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})
+--rollback MATCH (n:Movie {name: 'TheMatrix'})
+--rollback DELETE n
+
+-- changeset Tsvi:2
+CREATE (Keanu:Person {name:'Keanu Reeves', born:1964})
+--rollback MATCH (n:Person {name: 'Keanu'})
+--rollback DELETE n
+
+-- changeset Tsvi:3
+CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})
+--rollback MATCH (n:Person {name: 'Carrie'})
+--rollback DELETE n
+
+-- changeset Tsvi:4
+CREATE (Laurence:Person {name:'Laurence fishburne', born:1961})
+--rollback MATCH (n:Person {name: 'Laurence'})
+--rollback DELETE n
+
+-- changeset Tsvi:5
+CREATE (Hugo:Person {name:'Hugo weaving', born:1962})
+--rollback MATCH (n:Person {name: 'Hugo'})
+--rollback DELETE n
+
+-- changeset Tsvi:6
+CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})
+--rollback MATCH (n:Person {name: 'Lilly'})
+--rollback DELETE n
+
+-- changeset Tsvi:7
+CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})
+--rollback MATCH (n:Person {name: 'Lana'})
+--rollback DELETE n
+
+-- changeset Tsvi:8
+CREATE (JoelS:Person {name:'Joel Silver', born:1952})
+--rollback MATCH (n:Person {name: 'Joel'})
+--rollback DELETE n
+
+-- changeset Tsvi:9
+CREATE
+(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),
+(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),
+(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrix),
+(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrix),
+(LillyW)-[:DIRECTED]->(TheMatrix),
+(LanaW)-[:DIRECTED]->(TheMatrix),
+(JoelS)-[:PRODUCED]->(TheMatrix)
+-- rollback MATCH (n)
+-- rollback DETACH DELETE n
+
+-- changeset Tsvi:10
+GRANT SET USER HOME DATABASE
+
+-- changeset Tsvi:11
+CREATE ROLE mysecondrole IF NOT EXISTS AS COPY OF myrole
+
+-- changeset Tsvi:12
+REVOKE GRANT graph-privilege ON {HOME GRAPH | GRAPH[S] {* | name[, ...]}} [entity] FROM role[, ...]
