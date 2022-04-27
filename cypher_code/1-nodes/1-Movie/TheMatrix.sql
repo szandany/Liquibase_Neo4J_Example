827aee5dda91544e2cd0247177892851c662d24c
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2022-04-27 15:58:19 -0500

    Update TheMatrix.sql

--- cypher_code/1-nodes/1-Movie/TheMatrix.sql
+++ cypher_code/1-nodes/1-Movie/TheMatrix.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset Tsvi:3
-CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})
---rollback MATCH (n:Movie) WHERE n.title = 'The Matrix ${VERSION}'
+CREATE (TheMatrix:Movie {title:'The Matrix ${VERSION}', released:1999, tagline:'Welcome to the Real World'})
+--rollback MATCH (n:Movie) WHERE n.title = 'The Matrix'
 --rollback DELETE n
 
